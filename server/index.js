4731e61bfb8e1943bd653231448e10cb9b103e29
au_radimir <radimir@theforest.ru>
2021-07-05 15:49:05 +0500

    Начало создание бд и добавление методов управления данными

--- server/index.js
+++ server/index.js
@@ -3,8 +3,21 @@ const express = require('express')
 const sequelize = require('./db')
 const PORT = process.env.PORT || 5000
 const models = require('./models/models')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware') // должен регистрироваться последним т.к. он замыкающий
+const path = require('path')
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload({}))
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use('/api', router)
+// Обработка ошибок, последний middleware
+app.use(errorHandler)
 
 
 const start = async () =>{
--- server/models/models.js
+++ server/models/models.js
@@ -19,7 +19,7 @@ const BasketDevice = sequelize.define('basket_device',{
 const Device = sequelize.define('device',{
     id:{type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
     name:{type:DataTypes.STRING, unique:true,allowNull:false},
-    price:{type:DataTypes.INTEGER, unique:true,allowNull:false},
+    price:{type:DataTypes.INTEGER,allowNull:false},
     rating:{type:DataTypes.INTEGER,defaultValue:0},
     img:{type:DataTypes.STRING, unique:true,allowNull:false},
 })
@@ -71,7 +71,7 @@ Rating.belongsTo(Device)
 Device.hasMany(BasketDevice)
 BasketDevice.belongsTo(Device)
 
-Device.hasMany(DeviceInfo)
+Device.hasMany(DeviceInfo, {as: 'info'})
 DeviceInfo.belongsTo(Device)
 
 Type.belongsToMany(Brand, {through:TypeBrand})
