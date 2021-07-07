bd3e5537f8e26d292934310ed3ff30286f165ed3
au_radimir <radimir@theforest.ru>
2021-07-07 16:31:01 +0500

    Начало работы с фронтом

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Switch, Route, Redirect} from 'react-router-dom'
+import { authRotes, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
+
+const AppRouter = () =>{
+    const isAuth = false
+    return(
+        <Switch>
+            {isAuth && authRotes.map(({path, Component}) =>
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            {publicRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            <Redirect to={SHOP_ROUTE}/>
+        </Switch>
+    )
+}
+export default AppRouter
--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const TypeController = require('../controllers/typeController')
+const checRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/',TypeController.create)
+router.post('/',checRole('ADMIN'),TypeController.create)
 router.get('/',TypeController.getAll)
 
 module.exports = router
